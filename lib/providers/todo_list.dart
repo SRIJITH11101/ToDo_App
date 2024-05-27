f75750d8c8125b3f9ca80f94d848002d414962f8
SRIJITH11101 <dovetonsrijith29@gmail.com>
2024-05-27 15:25:02 +0530

    Todo list commit

---
+++ lib/providers/todo_list.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import 'package:todo_app/models/todo_model.dart';
+
+class TodoListState extends Equatable {
+  final List<Todo> todos;
+
+  TodoListState({required this.todos});
+
+  factory TodoListState.initial() {
+    return TodoListState(todos: [
+      Todo(id: '1', desc: 'Description 1'),
+      Todo(id: '2', desc: 'Description 1'),
+      Todo(id: '3', desc: 'Description 1'),
+    ]);
+  }
+
+  @override
+  List<Object> get props => [todos];
+
+  @override
+  bool get stringify => true;
+
+  TodoListState copyWith({
+    List<Todo>? todos,
+  }) {
+    return TodoListState(
+      todos: todos ?? this.todos,
+    );
+  }
+}
+
+class TodoList with ChangeNotifier {
+  TodoListState _state = TodoListState.initial();
+  TodoListState get state => _state;
+
+  void addTodo(String todoDesc) {
+    final newTodo = Todo(desc: todoDesc);
+    final newTodos = [..._state.todos, newTodo];
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void editTodo(String id, String todoDesc) {
+    final newTodos = _state.todos.map((Todo todo) {
+      if (todo.id == id) {
+        return Todo(
+          id: id,
+          desc: todoDesc,
+          completed: todo.completed,
+        );
+      }
+      return todo;
+    }).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+
+  void toggleTodo(String id) {
+    final newTodos = _state.todos.map((Todo todo) {
+      if (todo.id == id) {
+        return Todo(
+          id: id,
+          desc: todo.desc,
+          completed: !todo.completed,
+        );
+      }
+      return todo;
+    }).toList();
+
+    _state = _state.copyWith(todos: newTodos);
+    notifyListeners();
+  }
+}
